d413d6851e63096b5f75b5778688be5f50ffcdc9
devopMarkz <marcosdev2002@gmail.com>
2025-04-14 20:20:43 -0300

    Eventos de teclado com @keyup e @keypress

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,12 @@
   // import Exemplo04 from "./components/Exemplo04HTML.vue";
   // import Exemplo05 from "./components/Exemplo05Model.vue";
   // import Exemplo06 from './components/Exemplo06Classe.vue'
-  import Exemplo07 from './components/Exemplo07Click.vue'
+  // import Exemplo07 from './components/Exemplo07Click.vue'
+  import Exemplo08 from './components/Exemplo08Teclado.vue'
 </script>
 
 <!-- TEMPLATE -->
 <!-- HTML -->
 <template>
-  <Exemplo07></Exemplo07>
+  <Exemplo08></Exemplo08>
 </template>
---
+++ src/components/Exemplo08Teclado.vue
@@ -0,0 +1,9 @@
+<script setup>
+    function mostrarMensagem(event) {
+        alert(`Tecla pressionada: ${event.key}`);
+    } 
+</script>
+
+<template>
+    <input type="text" @keypress="mostrarMensagem" placeholder="Pressione uma tecla" />
+</template>
