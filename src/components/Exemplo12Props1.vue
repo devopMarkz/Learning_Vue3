ab03888a04538588cb0689554df2f18e9d990a46
devopMarkz <marcosdev2002@gmail.com>
2025-04-15 21:38:47 -0300

    Aprendendo sobre props dinamicas

--- src/components/Exemplo12Props1.vue
+++ src/components/Exemplo12Props1.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
     import Exemplo12Props2 from './Exemplo12Props2.vue';
+    const nome = ref<string>('Andrés');
+    const idade = ref<string>('22');
 </script>
 
 <template>
-    <Exemplo12Props2 nome="Marcos" idade="22"></Exemplo12Props2>
+    <Exemplo12Props2 :nome="nome" :idade="idade"></Exemplo12Props2>
 </template>
